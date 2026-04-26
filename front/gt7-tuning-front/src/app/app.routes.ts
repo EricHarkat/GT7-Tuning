52c824479cb65db7c9ad5603979381577fcf31a4
EricHarkat <eric.harkat@gmail.com>
2026-04-26 18:41:05 +0200

    feat: add home page and layout

--- front/gt7-tuning-front/src/app/app.routes.ts
+++ front/gt7-tuning-front/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { CarListComponent } from './pages/car-list/car-list.component';
 import { CarDetailComponent } from './pages/car-detail/car-detail.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'cars', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'cars', component: CarListComponent },
   { path: 'cars/:id', component: CarDetailComponent },
 ];
--- front/gt7-tuning-front/src/app/app.ts
+++ front/gt7-tuning-front/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatSidenavModule,
+    MatButtonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('gt7-tuning-front');
-}
+export class App {}
